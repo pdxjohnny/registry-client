ba23840992af8e68d7fb750f8630e5015f15c28b
John Andersen <johnandersenpdx@gmail.com>
2015-09-10 10:03:34 -0700

    Override process.env proxy variables with this_.config proxy variables so request.js respects them
    Signed-off-by: John Andersen <johnandersenpdx@gmail.com>

--- Client.js
+++ Client.js
@@ -12,9 +12,16 @@ function RegistryClient(config, logger) {
         this._config.cache = this._config.storage ? this._config.storage.registry : null;
     }
 
-    // In case config doesn't have no_proxy
-    if (!Object.prototype.hasOwnProperty.call(this._config, 'no_proxy')) {
-        this._config.no_proxy = process.env.no_proxy;
+    // Override environment defaults if proxy config options are set
+    // This will make requests follow the proxies in this._config
+    if (Object.prototype.hasOwnProperty.call(this._config, 'no_proxy')) {
+        process.env.no_proxy = this._config.no_proxy;
+    }
+    if (Object.prototype.hasOwnProperty.call(this._config, 'proxy')) {
+        process.env.http_proxy = this._config.proxy;
+    }
+    if (Object.prototype.hasOwnProperty.call(this._config, 'httpsProxy')) {
+        process.env.https_proxy = this._config.httpsProxy;
     }
 
     // Init the cache
